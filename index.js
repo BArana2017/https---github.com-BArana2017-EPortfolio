4c99114a79726fc9d6b7219467e44bc8b619cc66
Bianca <barana99@yahoo.com>
2025-09-10 16:07:18 -0500

    js email pt 1

---
+++ index.js
@@ -0,0 +1,20 @@
+function contact(event) {
+    event.preventDefault();
+    const loading = document.querySelector('.modal__overlay--loading');
+    const success = document.querySelector('.modal__overlay--success');
+    loading.classList += " modal__overlay--visible";
+    emailjs
+    .sendForm (
+        'service_auraybw',
+        'template_xyut2an',
+        event.target,
+        'UQbMcE40RNazkwACj'
+    ).then(() => {
+        loading.classList.remove("modal__overlay--visible")
+        success.classList += " modal__overlay--visible"
+    }).catch(() => {
+        loading.classList.remove("modal__overlay--visible")
+        alert(
+            "The email service is temporarily unavailable. Please contact me directly at aranab2017@gmail.com")
+})
+}
